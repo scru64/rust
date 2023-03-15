6995616816b91a6830daf5d76976a13faf422748
LiosK <contact@mail.liosk.net>
2023-03-15 21:46:32 +0900

    v0.1.0 Released

---
+++ src/generator.rs
@@ -0,0 +1,222 @@
+//! SCRU64 generator and related items.
+
+#[cfg(not(feature = "std"))]
+use core as std;
+use std::fmt;
+
+use pcg32::Pcg32;
+
+use crate::{Scru64Id, NODE_CTR_SIZE};
+
+/// Represents a SCRU64 ID generator.
+///
+/// The generator offers four different methods to generate a SCRU64 ID:
+///
+/// | Flavor                      | Timestamp | On big clock rewind |
+/// | --------------------------- | --------- | ------------------- |
+/// | [`generate`]                | Now       | Rewinds state       |
+/// | [`generate_no_rewind`]      | Now       | Returns `None`      |
+/// | [`generate_core`]           | Argument  | Rewinds state       |
+/// | [`generate_core_no_rewind`] | Argument  | Returns `None`      |
+///
+/// Each method returns monotonically increasing IDs unless a timestamp provided is significantly
+/// (by ~10 seconds or more) smaller than the one embedded in the immediately preceding ID. If such
+/// a significant clock rollback is detected, the standard `generate` rewinds the generator state
+/// and returns a new ID based on the current timestamp, whereas `no_rewind` variants keep the
+/// state untouched and return `None`. `core` functions offer low-level primitives.
+///
+/// [`generate`]: Scru64Generator::generate
+/// [`generate_no_rewind`]: Scru64Generator::generate_no_rewind
+/// [`generate_core`]: Scru64Generator::generate_core
+/// [`generate_core_no_rewind`]: Scru64Generator::generate_core_no_rewind
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Scru64Generator {
+    prev: Scru64Id,
+    counter_size: u8,
+    rng: Pcg32,
+}
+
+impl Scru64Generator {
+    /// Creates a generator with a node configuration.
+    ///
+    /// The `node_id` must fit in `node_id_size` bits, where `node_id_size` ranges from 1 to 23,
+    /// inclusive.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the arguments represent an invalid node configuration.
+    pub fn new(node_id: u32, node_id_size: u8) -> Self {
+        verify_node(node_id, node_id_size).unwrap_or_else(|err| panic!("{err}"));
+        let counter_size = NODE_CTR_SIZE - node_id_size;
+        let prev = Scru64Id::from_parts(0, node_id << counter_size);
+        let seed = &counter_size as *const u8; // use local var address as seed
+        Self {
+            prev,
+            counter_size,
+            rng: Pcg32::new(prev.to_u64(), seed as u64),
+        }
+    }
+
+    /// Creates a generator by parsing a node spec string that describes the node configuration.
+    ///
+    /// A node spec string consists of `node_id` and `node_id_size` separated by a slash (e.g.,
+    /// `"42/8"`, `"12345/16"`).
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the node spec does not conform to the valid syntax or represents an
+    /// invalid node configuration.
+    pub fn parse(node_spec: &str) -> Result<Self, NodeSpecError> {
+        const ERR: NodeSpecError = NodeSpecError::Syntax;
+        let mut iter = node_spec.split('/');
+        let node_id: u32 = iter.next().ok_or(ERR)?.parse().or(Err(ERR))?;
+        let node_id_size: u8 = iter.next().ok_or(ERR)?.parse().or(Err(ERR))?;
+        if iter.next().is_some() {
+            return Err(ERR);
+        }
+
+        verify_node(node_id, node_id_size)?;
+        Ok(Self::new(node_id, node_id_size))
+    }
+
+    /// Returns the `node_id` of the generator.
+    pub const fn node_id(&self) -> u32 {
+        self.prev.node_ctr() >> self.counter_size
+    }
+
+    /// Returns the size in bits of the `node_id` adopted by the generator.
+    pub const fn node_id_size(&self) -> u8 {
+        NODE_CTR_SIZE - self.counter_size
+    }
+
+    /// Calculates the combined `node_ctr` field value for the next `timestamp` tick.
+    fn init_node_ctr(&mut self) -> u32 {
+        // initialize counter at `counter_size - 1`-bit random number
+        const OVERFLOW_GUARD_SIZE: u8 = 1;
+        let counter = self.rng.gen() >> (32 + OVERFLOW_GUARD_SIZE - self.counter_size);
+
+        (self.node_id() << self.counter_size) | counter
+    }
+
+    /// Generates a new SCRU64 ID object from a Unix timestamp in milliseconds.
+    ///
+    /// See the [`Scru64Generator`] type documentation for the description.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the argument is not a positive integer within the valid range.
+    pub fn generate_core(&mut self, unix_ts_ms: u64) -> Scru64Id {
+        if let Some(value) = self.generate_core_no_rewind(unix_ts_ms) {
+            value
+        } else {
+            // reset state and resume
+            self.prev = Scru64Id::from_parts(unix_ts_ms >> 8, self.init_node_ctr());
+            self.prev
+        }
+    }
+
+    /// Generates a new SCRU64 ID object from a Unix timestamp in milliseconds, guaranteeing the
+    /// monotonic order of generated IDs despite a significant timestamp rollback.
+    ///
+    /// See the [`Scru64Generator`] type documentation for the description.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the argument is not a positive integer within the valid range.
+    pub fn generate_core_no_rewind(&mut self, unix_ts_ms: u64) -> Option<Scru64Id> {
+        const ROLLBACK_ALLOWANCE: u64 = 40; // x256 milliseconds = ~10 seconds
+
+        let timestamp = unix_ts_ms >> 8;
+        assert!(timestamp > 0, "`timestamp` out of range");
+
+        let prev_timestamp = self.prev.timestamp();
+        if timestamp > prev_timestamp {
+            self.prev = Scru64Id::from_parts(timestamp, self.init_node_ctr());
+        } else if timestamp + ROLLBACK_ALLOWANCE > prev_timestamp {
+            // go on with previous timestamp if new one is not much smaller
+            let prev_node_ctr = self.prev.node_ctr();
+            let counter_mask = (1u32 << self.counter_size) - 1;
+            if (prev_node_ctr & counter_mask) < counter_mask {
+                self.prev = Scru64Id::from_parts(prev_timestamp, prev_node_ctr + 1);
+            } else {
+                // increment timestamp at counter overflow
+                self.prev = Scru64Id::from_parts(prev_timestamp + 1, self.init_node_ctr());
+            }
+        } else {
+            // abort if clock moves back to unbearable extent
+            return None;
+        }
+        Some(self.prev)
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+mod std_ext {
+    use super::{Scru64Generator, Scru64Id};
+
+    /// Returns the current Unix timestamp in milliseconds.
+    fn unix_ts_ms() -> u64 {
+        use std::time;
+        time::SystemTime::now()
+            .duration_since(time::UNIX_EPOCH)
+            .expect("clock may have gone backward")
+            .as_millis() as u64
+    }
+
+    impl Scru64Generator {
+        /// Generates a new SCRU64 ID object from the current `timestamp`.
+        ///
+        /// See the [`Scru64Generator`] type documentation for the description.
+        pub fn generate(&mut self) -> Scru64Id {
+            self.generate_core(unix_ts_ms())
+        }
+
+        /// Generates a new SCRU64 ID object from the current `timestamp`, guaranteeing the
+        /// monotonic order of generated IDs despite a significant timestamp rollback.
+        ///
+        /// See the [`Scru64Generator`] type documentation for the description.
+        pub fn generate_no_rewind(&mut self) -> Option<Scru64Id> {
+            self.generate_core_no_rewind(unix_ts_ms())
+        }
+    }
+}
+
+/// Tests if a pair of `node_id` and `node_id_size` is valid.
+const fn verify_node(node_id: u32, node_id_size: u8) -> Result<(), NodeSpecError> {
+    if node_id_size == 0 || node_id_size >= NODE_CTR_SIZE {
+        return Err(NodeSpecError::NodeSizeRange);
+    }
+    if (node_id >> node_id_size) > 0 {
+        return Err(NodeSpecError::NodeRange);
+    }
+    Ok(())
+}
+
+/// Error parsing a node spec string.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[non_exhaustive]
+pub enum NodeSpecError {
+    /// Invalid node spec syntax.
+    Syntax,
+
+    /// `node_id` is greater than the `node_id_size` range.
+    NodeRange,
+
+    /// `node_id_size` is zero or greater than 23.
+    NodeSizeRange,
+}
+
+impl fmt::Display for NodeSpecError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Syntax => f.write_str("invalid `node_spec`; it looks like: `42/8`, `12345/16`"),
+            Self::NodeRange => f.write_str("`node_id` must fit in `node_id_size` bits"),
+            Self::NodeSizeRange => f.write_str("`node_id_size` must range from 1 to 23"),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+impl std::error::Error for NodeSpecError {}
---
+++ src/identifier.rs
@@ -0,0 +1,304 @@
+#[cfg(not(feature = "std"))]
+use core as std;
+use std::{fmt, str};
+
+use fstr::FStr;
+
+use crate::NODE_CTR_SIZE;
+
+/// Maximum valid value of the `timestamp` field.
+const TIMESTAMP_MAX: u64 = Scru64Id::MAX.to_u64() >> NODE_CTR_SIZE;
+
+/// Maximum valid value of the combined `node_ctr` field.
+const NODE_CTR_MAX: u32 = (1 << NODE_CTR_SIZE) - 1;
+
+/// Digit characters used in the Base36 notation.
+const DIGITS: [u8; 36] = *b"0123456789abcdefghijklmnopqrstuvwxyz";
+
+/// O(1) map from ASCII code points to Base36 digit values.
+const DECODE_MAP: [u8; 256] = [
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+    0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+    0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+];
+
+/// Represents a SCRU64 ID.
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+#[repr(transparent)]
+pub struct Scru64Id(u64);
+
+impl Scru64Id {
+    /// Minimum valid value (i.e., `000000000000`).
+    pub const MIN: Self = Scru64Id(0u64);
+
+    /// Maximum valid value (i.e., `zzzzzzzzzzzz`).
+    pub const MAX: Self = Scru64Id(36u64.pow(12) - 1);
+
+    /// Returns the integer representation.
+    pub const fn to_u64(self) -> u64 {
+        self.0
+    }
+
+    /// Creates a value from a 64-bit integer in the `const` context.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the argument is out of the valid value range.
+    pub const fn const_from_u64(value: u64) -> Self {
+        match Self::try_from_u64(value) {
+            Ok(t) => t,
+            Err(_) => panic!("out of valid integer range"),
+        }
+    }
+
+    /// Creates a value from a 64-bit integer in the `const` context.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the argument is out of the valid value range.
+    const fn try_from_u64(value: u64) -> Result<Self, ConversionError> {
+        if value <= Self::MAX.0 {
+            Ok(Self(value))
+        } else {
+            Err(ConversionError::OutOfRange)
+        }
+    }
+
+    /// Returns the 12-digit canonical string representation stored in a stack-allocated
+    /// string-like type that can be handled like [`String`] through common traits.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use scru64::Scru64Id;
+    ///
+    /// let x = "0u2r87q2rol5".parse::<Scru64Id>()?;
+    /// let y = x.encode();
+    /// assert_eq!(y, "0u2r87q2rol5");
+    /// assert_eq!(format!("{y}"), "0u2r87q2rol5");
+    /// # Ok::<(), scru64::ConversionError>(())
+    /// ```
+    pub const fn encode(self) -> FStr<12> {
+        let mut buffer = [0u8; 12];
+        let mut n = self.0;
+        let mut i = buffer.len();
+        while i > 0 {
+            i -= 1;
+            let (quo, rem) = (n / 36, n % 36);
+            buffer[i] = DIGITS[rem as usize];
+            n = quo;
+        }
+        // SAFETY: ok because buffer consists of ASCII code points
+        debug_assert!(str::from_utf8(&buffer).is_ok());
+        unsafe { FStr::from_inner_unchecked(buffer) }
+    }
+
+    /// Creates a value from a 12-digit string representation in the `const` context.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the argument is not a valid string representation.
+    pub const fn const_from_str(value: &str) -> Self {
+        match Self::try_from_str(value) {
+            Ok(t) => t,
+            Err(_) => panic!("invalid string representation"),
+        }
+    }
+
+    /// Creates a value from a 12-digit string representation in the `const` context.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the argument is not a valid string representation.
+    const fn try_from_str(value: &str) -> Result<Self, ConversionError> {
+        if value.len() != 12 {
+            Err(ConversionError::InvalidLength)
+        } else {
+            let buffer = value.as_bytes();
+            let mut n = 0u64;
+            let mut i = 0;
+            while i < value.len() {
+                let e = DECODE_MAP[buffer[i] as usize];
+                if e == 0xff {
+                    return Err(ConversionError::InvalidDigit);
+                }
+                n = n * 36 + e as u64;
+                i += 1;
+            }
+            Self::try_from_u64(n)
+        }
+    }
+
+    /// Returns the `timestamp` field value.
+    pub const fn timestamp(self) -> u64 {
+        self.0 >> NODE_CTR_SIZE
+    }
+
+    /// Returns the `node_id` and `counter` field values combined as a single integer.
+    pub const fn node_ctr(self) -> u32 {
+        self.0 as u32 & NODE_CTR_MAX
+    }
+
+    /// Creates a value from the `timestamp` and the combined `node_ctr` field value.
+    ///
+    /// # Panics
+    ///
+    /// Panics if any argument is out of the valid value range.
+    pub const fn from_parts(timestamp: u64, node_ctr: u32) -> Self {
+        assert!(timestamp <= TIMESTAMP_MAX, "`timestamp` out of range");
+        assert!(node_ctr <= NODE_CTR_MAX, "`node_ctr` out of range");
+        Self(timestamp << NODE_CTR_SIZE | node_ctr as u64)
+    }
+}
+
+impl From<Scru64Id> for u64 {
+    fn from(value: Scru64Id) -> Self {
+        value.to_u64()
+    }
+}
+
+impl TryFrom<u64> for Scru64Id {
+    type Error = ConversionError;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        Self::try_from_u64(value)
+    }
+}
+
+impl From<Scru64Id> for i64 {
+    fn from(value: Scru64Id) -> Self {
+        value.to_u64() as i64
+    }
+}
+
+impl TryFrom<i64> for Scru64Id {
+    type Error = ConversionError;
+
+    fn try_from(value: i64) -> Result<Self, Self::Error> {
+        // cast negative numbers to so large numbers that try_from_u64 rejects
+        Self::try_from(value as u64)
+    }
+}
+
+impl fmt::Display for Scru64Id {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.encode())
+    }
+}
+
+impl str::FromStr for Scru64Id {
+    type Err = ConversionError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        Self::try_from_str(value)
+    }
+}
+
+/// Error converting a value into SCRU64 ID.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[non_exhaustive]
+pub enum ConversionError {
+    /// Out of valid integer range.
+    OutOfRange,
+
+    /// Invalid length of string.
+    InvalidLength,
+
+    /// Invalid digit char found.
+    InvalidDigit,
+}
+
+impl fmt::Display for ConversionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("could not convert to SCRU64 ID: ")?;
+        match self {
+            Self::OutOfRange => f.write_str("out of valid integer range"),
+            Self::InvalidLength => f.write_str("invalid length of string"),
+            Self::InvalidDigit => f.write_str("invalid digit char found"),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+mod std_ext {
+    use super::{ConversionError, Scru64Id};
+
+    impl TryFrom<String> for Scru64Id {
+        type Error = ConversionError;
+
+        fn try_from(value: String) -> Result<Self, Self::Error> {
+            value.parse()
+        }
+    }
+
+    impl From<Scru64Id> for String {
+        fn from(object: Scru64Id) -> Self {
+            object.encode().into()
+        }
+    }
+
+    impl std::error::Error for ConversionError {}
+}
+
+#[cfg(feature = "serde")]
+#[cfg_attr(docsrs, doc(cfg(feature = "serde")))]
+mod serde_support {
+    use super::{fmt, Scru64Id};
+    use serde::{de, Deserializer, Serializer};
+
+    impl serde::Serialize for Scru64Id {
+        fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+            if serializer.is_human_readable() {
+                serializer.serialize_str(&self.encode())
+            } else {
+                serializer.serialize_u64(self.to_u64())
+            }
+        }
+    }
+
+    impl<'de> serde::Deserialize<'de> for Scru64Id {
+        fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+            if deserializer.is_human_readable() {
+                deserializer.deserialize_str(VisitorImpl)
+            } else {
+                deserializer.deserialize_u64(VisitorImpl)
+            }
+        }
+    }
+
+    struct VisitorImpl;
+
+    impl<'de> de::Visitor<'de> for VisitorImpl {
+        type Value = Scru64Id;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(formatter, "a SCRU64 ID representation")
+        }
+
+        fn visit_str<E: de::Error>(self, value: &str) -> Result<Self::Value, E> {
+            value.parse().map_err(de::Error::custom)
+        }
+
+        fn visit_u64<E: de::Error>(self, value: u64) -> Result<Self::Value, E> {
+            Self::Value::try_from(value).map_err(de::Error::custom)
+        }
+
+        fn visit_i64<E: de::Error>(self, value: i64) -> Result<Self::Value, E> {
+            Self::Value::try_from(value).map_err(de::Error::custom)
+        }
+    }
+}
