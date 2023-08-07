3b80a5b9a025f45d39c6d37d8d6792adf844780a
LiosK <contact@mail.liosk.net>
2023-08-07 20:33:32 +0900

    refactor!: rewrite generator initialization code with NodeSpec

--- src/generator.rs
+++ src/generator.rs
@@ -1,9 +1,5 @@
 //! SCRU64 generator and related items.
 
-#[cfg(not(feature = "std"))]
-use core as std;
-use std::fmt;
-
 use crate::{Scru64Id, NODE_CTR_SIZE};
 
 pub mod node_spec;
@@ -50,86 +46,18 @@ pub struct Scru64Generator<C = PartialRandom> {
 impl Scru64Generator {
     /// Creates a new generator with the given node configuration.
     ///
-    /// The `node_id` must fit in `node_id_size` bits, where `node_id_size` ranges from 1 to 23,
-    /// inclusive.
+    /// # Examples
     ///
-    /// # Panics
-    ///
-    /// Panics if the arguments represent an invalid node configuration.
-    pub fn new(node_id: u32, node_id_size: u8) -> Self {
-        verify_node(node_id, node_id_size).unwrap_or_else(|err| panic!("{err}"));
-        Self::restore(
-            Scru64Id::from_parts(0, node_id << (NODE_CTR_SIZE - node_id_size)),
-            node_id_size,
-        )
-    }
-
-    /// Restores the generator state from the immediately preceding SCRU64 ID and `node_id_size`
-    /// parameter.
-    ///
-    /// The returned generator refers to `node_prev` as the source of `node_id` and as the
-    /// preceding ID when generating a monotonically increasing ID.
-    ///
-    /// # Panics
-    ///
-    /// Panics if `node_id_size` is zero or greater than 23.
-    pub fn restore(node_prev: Scru64Id, node_id_size: u8) -> Self {
-        verify_node_id_size(node_id_size).unwrap_or_else(|err| panic!("{err}"));
-        Self {
-            prev: node_prev,
-            counter_size: NODE_CTR_SIZE - node_id_size,
-            counter_mode: Default::default(),
-        }
-    }
-
-    /// Creates a generator by parsing a node spec string that describes the node configuration.
-    ///
-    /// A node spec string starts with a decimal `node_id` or 12-digit `node_prev` value, followed
-    /// by a slash and a decimal `node_id_size` value ranging from 1 to 23 (e.g., `"42/8"`,
-    /// `"0u2r85hm2pt3/16"`). The first form creates a fresh new generator with the given `node_id`,
-    /// while the second form constructs one that generates subsequent IDs to the `node_prev`.
-    ///
-    /// # Errors
-    ///
-    /// Returns `Err` if the node spec does not conform to the valid syntax or represents an
-    /// invalid node configuration.
-    pub fn parse(node_spec: &str) -> Result<Self, NodeSpecError> {
-        use NodeSpecError::Syntax;
-        let mut iter = node_spec.split('/');
-        let x = iter.next().ok_or(Syntax)?;
-        let y = iter.next().ok_or(Syntax)?;
-        if iter.next().is_some()
-            || x.starts_with('+')
-            || x.len() > 12
-            || y.starts_with('+')
-            || y.len() > 3
-        {
-            return Err(Syntax);
-        }
-
-        let node_id_size: u8 = y.parse().or(Err(Syntax))?;
-        if x.len() == 12 {
-            let node_prev = x.parse().or(Err(Syntax))?;
-            verify_node_id_size(node_id_size)?;
-            Ok(Self::restore(node_prev, node_id_size))
-        } else {
-            let node_id = x.parse().or(Err(Syntax))?;
-            verify_node(node_id, node_id_size)?;
-            Ok(Self::new(node_id, node_id_size))
-        }
+    /// ```rust
+    /// # use scru64::generator::Scru64Generator;
+    /// let g = Scru64Generator::new("42/8".parse().unwrap());
+    /// ```
+    pub const fn new(node_spec: NodeSpec) -> Self {
+        Self::with_counter_mode(node_spec, PartialRandom::new(1))
     }
 }
 
 impl<C> Scru64Generator<C> {
-    /// Returns the immediately preceding ID that the generator generated.
-    pub const fn node_prev(&self) -> Option<Scru64Id> {
-        if self.prev.timestamp() > 0 {
-            Some(self.prev)
-        } else {
-            None
-        }
-    }
-
     /// Returns the `node_id` of the generator.
     pub const fn node_id(&self) -> u32 {
         self.prev.node_ctr() >> self.counter_size
@@ -140,13 +68,11 @@ impl<C> Scru64Generator<C> {
         NODE_CTR_SIZE - self.counter_size
     }
 
-    /// Writes the node spec string describing `self` into a `buffer`.
-    #[cfg_attr(not(feature = "std"), allow(dead_code))]
-    fn write_node_spec(&self, buffer: &mut impl fmt::Write) -> fmt::Result {
-        if let Some(prev) = self.node_prev() {
-            write!(buffer, "{}/{}", prev, self.node_id_size())
-        } else {
-            write!(buffer, "{}/{}", self.node_id(), self.node_id_size())
+    /// Returns the node configuration specifier describing the generator state.
+    pub fn node_spec(&self) -> NodeSpec {
+        match NodeSpec::with_node_prev(self.prev, self.node_id_size()) {
+            Ok(t) => t,
+            Err(_) => unreachable!(),
         }
     }
 }
@@ -259,17 +185,6 @@ mod std_ext {
             .as_millis() as u64
     }
 
-    impl<C> Scru64Generator<C> {
-        /// Returns the node spec string that revives the current generator state through
-        /// [`Scru64Generator::parse`].
-        pub fn node_spec(&self) -> String {
-            let mut buffer = String::new();
-            self.write_node_spec(&mut buffer)
-                .expect("could not write `node_spec` into `String`");
-            buffer
-        }
-    }
-
     impl<C: InitCounter> Scru64Generator<C> {
         /// Generates a new SCRU64 ID object from the current `timestamp`, or returns `None` upon
         /// significant timestamp rollback.
@@ -289,126 +204,10 @@ mod std_ext {
     }
 }
 
-/// Tests if `node_id_size` is within the valid value range.
-const fn verify_node_id_size(node_id_size: u8) -> Result<(), NodeSpecError> {
-    if 0 < node_id_size && node_id_size < NODE_CTR_SIZE {
-        Ok(())
-    } else {
-        Err(NodeSpecError::NodeIdSizeRange)
-    }
-}
-
-/// Tests if a pair of `node_id` and `node_id_size` is valid.
-const fn verify_node(node_id: u32, node_id_size: u8) -> Result<(), NodeSpecError> {
-    match verify_node_id_size(node_id_size) {
-        Ok(_) => match node_id >> node_id_size {
-            0 => Ok(()),
-            _ => Err(NodeSpecError::NodeIdRange),
-        },
-        err => err,
-    }
-}
-
-/// An error parsing a node spec string.
-#[derive(Clone, Debug, Eq, PartialEq)]
-#[non_exhaustive]
-pub enum NodeSpecError {
-    /// Invalid node spec syntax.
-    Syntax,
-
-    /// `node_id` is greater than the `node_id_size` range.
-    NodeIdRange,
-
-    /// `node_id_size` is zero or greater than 23.
-    NodeIdSizeRange,
-}
-
-impl fmt::Display for NodeSpecError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Syntax => {
-                f.write_str("invalid `node_spec`; it looks like: `42/8`, `0u2r85hm2pt3/16`")
-            }
-            Self::NodeIdRange => f.write_str("`node_id` must fit in `node_id_size` bits"),
-            Self::NodeIdSizeRange => f.write_str("`node_id_size` must range from 1 to 23"),
-        }
-    }
-}
-
-#[cfg(feature = "std")]
-#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
-impl std::error::Error for NodeSpecError {}
-
 #[cfg(test)]
 mod tests {
-    use super::{Scru64Generator, Scru64Id};
+    use super::{NodeSpec, Scru64Generator, Scru64Id};
     use crate::test_cases::EXAMPLE_NODE_SPECS;
-
-    /// Initializes with node ID and size pair and node spec string.
-    #[test]
-    fn constructor() {
-        for e in EXAMPLE_NODE_SPECS {
-            let node_prev = Scru64Id::const_from_u64(e.node_prev);
-
-            let newed = Scru64Generator::new(e.node_id, e.node_id_size);
-            assert_eq!(newed.node_id(), e.node_id);
-            assert_eq!(newed.node_id_size(), e.node_id_size);
-            if e.spec_type == "new" {
-                assert_eq!(newed.prev, node_prev);
-            }
-
-            let parsed = Scru64Generator::parse(e.node_spec).unwrap();
-            assert_eq!(parsed.node_id(), e.node_id);
-            assert_eq!(parsed.node_id_size(), e.node_id_size);
-            assert_eq!(parsed.prev, node_prev);
-
-            let restored = Scru64Generator::restore(node_prev, e.node_id_size);
-            assert_eq!(restored.node_id(), e.node_id);
-            assert_eq!(restored.node_id_size(), e.node_id_size);
-            assert_eq!(restored.prev, node_prev);
-
-            #[cfg(feature = "std")]
-            {
-                if e.spec_type == "new" {
-                    assert_eq!(newed.node_spec(), e.node_spec);
-                }
-                assert_eq!(parsed.node_spec(), e.node_spec);
-                assert_eq!(restored.node_spec(), e.node_spec);
-            }
-        }
-    }
-
-    /// Fails to initialize with invalid node spec string.
-    #[test]
-    fn constructor_error() {
-        let cases = [
-            "",
-            "42",
-            "/8",
-            "42/",
-            " 42/8",
-            "42/8 ",
-            " 42/8 ",
-            "42 / 8",
-            "+42/8",
-            "42/+8",
-            "-42/8",
-            "42/-8",
-            "ab/8",
-            "0x42/8",
-            "1/2/3",
-            "0/0",
-            "0/24",
-            "8/1",
-            "1024/8",
-            "0000000000001/8",
-            "1/0016",
-        ];
-
-        for e in cases {
-            assert!(Scru64Generator::parse(e).is_err());
-        }
-    }
 
     fn test_consecutive_pair(first: Scru64Id, second: Scru64Id) {
         assert!(first < second);
@@ -427,7 +226,8 @@ mod tests {
 
         for e in EXAMPLE_NODE_SPECS {
             let counter_size = 24 - e.node_id_size;
-            let mut g = Scru64Generator::new(e.node_id, e.node_id_size);
+            let node_spec = NodeSpec::with_node_id(e.node_id, e.node_id_size).unwrap();
+            let mut g = Scru64Generator::new(node_spec);
 
             // happy path
             let mut ts = 1_577_836_800_000u64; // 2020-01-01
@@ -478,7 +278,8 @@ mod tests {
 
         for e in EXAMPLE_NODE_SPECS {
             let counter_size = 24 - e.node_id_size;
-            let mut g = Scru64Generator::new(e.node_id, e.node_id_size);
+            let node_spec = NodeSpec::with_node_id(e.node_id, e.node_id_size).unwrap();
+            let mut g = Scru64Generator::new(node_spec);
 
             // happy path
             let mut ts = 1_577_836_800_000u64; // 2020-01-01
@@ -531,7 +332,8 @@ mod tests {
         }
 
         for e in EXAMPLE_NODE_SPECS {
-            let mut g = Scru64Generator::new(e.node_id, e.node_id_size);
+            let node_spec = NodeSpec::with_node_id(e.node_id, e.node_id_size).unwrap();
+            let mut g = Scru64Generator::new(node_spec);
             let mut ts_now = now();
             let mut x = g.generate().unwrap();
             assert!((x.timestamp() - ts_now) <= 1);
@@ -539,87 +341,6 @@ mod tests {
             ts_now = now();
             x = g.generate_or_reset();
             assert!((x.timestamp() - ts_now) <= 1);
-        }
-    }
-}
-
-#[cfg(feature = "serde")]
-#[cfg_attr(docsrs, doc(cfg(feature = "serde")))]
-mod serde_support {
-    #[cfg(not(feature = "std"))]
-    use core as std;
-    use std::{fmt, str};
-
-    use super::Scru64Generator;
-    use serde::{de, Deserializer, Serializer};
-
-    impl serde::Serialize for Scru64Generator {
-        fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-            let mut buffer = fstr::FStr::<16>::repeat(b'\0');
-            self.write_node_spec(&mut buffer.writer()).unwrap();
-            serializer.serialize_str(buffer.slice_to_terminator('\0'))
-        }
-    }
-
-    impl<'de> serde::Deserialize<'de> for Scru64Generator {
-        fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-            deserializer.deserialize_str(VisitorImpl)
-        }
-    }
-
-    struct VisitorImpl;
-
-    impl<'de> de::Visitor<'de> for VisitorImpl {
-        type Value = Scru64Generator;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-            write!(formatter, "a SCRU64 node spec string")
-        }
-
-        fn visit_str<E: de::Error>(self, value: &str) -> Result<Self::Value, E> {
-            Self::Value::parse(value).map_err(de::Error::custom)
-        }
-
-        fn visit_bytes<E: de::Error>(self, value: &[u8]) -> Result<Self::Value, E> {
-            match str::from_utf8(value) {
-                Ok(str_value) => self.visit_str(str_value),
-                _ => Err(de::Error::invalid_value(
-                    de::Unexpected::Bytes(value),
-                    &self,
-                )),
-            }
-        }
-    }
-
-    /// Supports serialization and deserialization.
-    #[cfg(test)]
-    #[test]
-    fn test() {
-        use serde::{Deserialize, Serialize};
-        use serde_test::Token;
-
-        #[derive(Debug)]
-        struct CustomEqWrapper(Scru64Generator);
-        impl PartialEq for CustomEqWrapper {
-            fn eq(&self, other: &CustomEqWrapper) -> bool {
-                self.0.prev == other.0.prev && self.0.counter_size == other.0.counter_size
-            }
-        }
-        impl Serialize for CustomEqWrapper {
-            fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-                Scru64Generator::serialize(&self.0, serializer)
-            }
-        }
-        impl<'de> Deserialize<'de> for CustomEqWrapper {
-            fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-                Ok(CustomEqWrapper(Scru64Generator::deserialize(deserializer)?))
-            }
-        }
-
-        for e in crate::test_cases::EXAMPLE_NODE_SPECS {
-            let x = CustomEqWrapper(Scru64Generator::parse(e.node_spec).unwrap());
-            serde_test::assert_tokens(&x, &[Token::Str(e.node_spec)]);
-            serde_test::assert_de_tokens(&x, &[Token::Bytes(e.node_spec.as_bytes())]);
         }
     }
 }
--- src/generator/global_gen.rs
+++ src/generator/global_gen.rs
@@ -1,6 +1,6 @@
 use std::{env, sync};
 
-use super::{Scru64Generator, Scru64Id};
+use super::{NodeSpec, Scru64Generator, Scru64Id};
 
 /// A zero-sized type that forwards supported method calls to the process-wide global generator.
 ///
@@ -10,7 +10,9 @@ use super::{Scru64Generator, Scru64Id};
 /// # use scru64::generator::GlobalGenerator;
 /// std::env::set_var("SCRU64_NODE_SPEC", "42/8");
 ///
-/// assert_eq!(GlobalGenerator.node_spec(), "42/8");
+/// assert_eq!(GlobalGenerator.node_id(), 42);
+/// assert_eq!(GlobalGenerator.node_id_size(), 8);
+/// assert_eq!(GlobalGenerator.node_spec().to_string(), "42/8");
 /// ```
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct GlobalGenerator;
@@ -21,10 +23,10 @@ impl GlobalGenerator {
 
         G.get_or_init(|| {
             let node_spec = env::var("SCRU64_NODE_SPEC")
-                .expect("scru64: could not read config from SCRU64_NODE_SPEC env var");
-            let g = Scru64Generator::parse(&node_spec)
+                .expect("scru64: could not read config from SCRU64_NODE_SPEC env var")
+                .parse()
                 .expect("scru64: could not initialize global generator");
-            sync::Mutex::new(g)
+            sync::Mutex::new(Scru64Generator::new(node_spec))
         })
         .lock()
         .expect("scru64: could not lock global generator")
@@ -33,11 +35,6 @@ impl GlobalGenerator {
     /// Calls [`Scru64Generator::generate`] of the global generator.
     pub fn generate(&self) -> Option<Scru64Id> {
         self.lock().generate()
-    }
-
-    /// Calls [`Scru64Generator::node_prev`] of the global generator.
-    pub fn node_prev(&self) -> Option<Scru64Id> {
-        self.lock().node_prev()
     }
 
     /// Calls [`Scru64Generator::node_id`] of the global generator.
@@ -51,7 +48,7 @@ impl GlobalGenerator {
     }
 
     /// Calls [`Scru64Generator::node_spec`] of the global generator.
-    pub fn node_spec(&self) -> String {
+    pub fn node_spec(&self) -> NodeSpec {
         self.lock().node_spec()
     }
 }
